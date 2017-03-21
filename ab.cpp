04f4b4ab93ee526979dcbf6847c8a5cf7b69ad07
Vrubi <vrbovszkibence@gmail.com>
2017-03-21 11:20:26 +0100

    lab07

---
+++ ab.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "ab.h"
+using namespace std;
+
+A::A(const A& ar):i_adat(new int){
+    this->i_adat=ar.i_adat;
+    cout<<"\tA copy\n"<<endl;
+}
+
+B::B(const B& br):i_adat(new int),f_adat(new float){
+    this->i_adat=br.i_adat;
+    this->f_adat=br.f_adat;
+    cout<<"\tA copy\n"<<endl;
+}
